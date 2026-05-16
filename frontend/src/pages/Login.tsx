268076def056acc169f13b5badad8e605e8c0152
agileorc <graciellavienenta@gmail.com>
2026-05-17 00:44:30 +0700

    fix: improve responsive layout for lg and xl breakpoints on login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom'
 
 function Login () {
     return (
-        <main className='flex min-h-screen bg-linear-to-br from-[#0EB8DF] to-[#0E5998]'>
-            {/* Left Side */}
-            <section className='flex w-4/7 flex-col justify-center lg:gap-y-40 xl:gap-y-36 lg:px-8 lg:py-10 xl:px-14 xl:py-12 text-white'>
-                {/* Logo */}
-                <div>
-                    <img
-                        src={logo}
-                        alt='Vault of Evidence Logo'
-                        className='lg:max-h-18 xl:max-h-28'
-                    />
-                </div>
+        <main className='flex min-h-screen items-center bg-linear-to-br from-[#0EB8DF] to-[#0E5998]'>
+            {/* Logo */}
+            <div className='absolute w-md lg:top-10 lg:left-10 xl:top-12 xl:left-14'>
+                <img
+                    src={logo}
+                    alt='Vault of Evidence Logo'
+                    className='md:max-h-12 lg:max-h-18 xl:max-h-28'
+                />
+            </div>
 
+            {/* Left Side */}
+            <section className='flex w-4/7 flex-col justify-center lg:gap-y-40 lg:px-8 xl:px-14 text-white'>
                 {/* Text */}
                 <div className= 'flex flex-1 flex-col lg:px-5 xl:px-6 lg:gap-y-3 xl:gap-y-4'>
                     <h1 className='lg:text-4xl xl:text-5xl font-semibold font-montserrat leading-tight'>
@@ -28,32 +28,32 @@ function Login () {
             </section>
 
             {/* Right Side*/}
-            <section className='flex w-3/7 items-center justify-center px-20'>
-                <div className='flex flex-col gap-y-10 px-14 py-16 w-xl max-w-xl rounded-[40px] border border-white/40 bg-linear-to-br from-white/20 to-white/10 shadow-lg shadow-black/5 backdrop-blur-md'>
+            <section className='flex w-3/7 items-center lg:px-12 xl:px-20'>
+                <div className='flex flex-col lg:gap-y-6 xl:gap-y-10 lg:px-10 lg:py-12 xl:px-14 xl:py-16 w-xl max-w-xl lg:rounded-[36px] xl:rounded-[40px] border border-white/40 bg-linear-to-br from-white/20 to-white/10 shadow-lg shadow-black/5 backdrop-blur-md'>
                     <div className='flex flex-col'>
-                        <h2 className='text-[2.5rem] font-montserrat font-bold text-white'>
+                        <h2 className='lg:text-3xl xl:text-[2.5rem] font-montserrat font-bold text-white'>
                             Welcome Back!
                         </h2>
 
-                        <p className='font-montserrat font-medium text-white text-lg'>
+                        <p className='font-montserrat font-medium text-white lg:text-sm xl:text-lg'>
                             Continue where you left off.
                         </p>
                     </div>
-
+                    
                     {/* Email */}
-                    <div className='flex flex-col gap-y-1 text-lg'>
-                        <label className='block font-montserrat font-medium text-white'>
+                    <div className='flex flex-col gap-y-1 lg:text-sm xl:text-lg'>
+                        <label className='font-montserrat font-medium text-white'>
                             Email address
                         </label>
 
                         <input
                             type='email'
                             placeholder='youremail@mail.com'
-                            className='w-full rounded-xl border border-[#27D6FF] bg-[#002C49]/50 px-4 py-3 text-white outline-none'/>
+                            className='w-full lg:rounded-lg xl:rounded-xl border border-[#27D6FF] bg-[#002C49]/50 lg:px-2 lg:py-1.5 xl:px-4 xl:py-3 text-white outline-none'/>
                     </div>
 
                     {/* Password */}
-                    <div className='flex flex-col gap-y-1 text-lg'>
+                    <div className='flex flex-col gap-y-1 lg:text-sm xl:text-lg'>
                         <div className='flex items-center justify-between'>
                             
                             <label className='font-montserrat font-medium text-white'>
@@ -71,12 +71,12 @@ function Login () {
                         <input
                             type='password'
                             placeholder='******'
-                            className='w-full rounded-xl border border-[#27D6FF] bg-[#002C49]/50 px-4 py-3 text-white outline-none' 
+                            className='w-full lg:rounded-lg xl:rounded-xl border border-[#27D6FF] bg-[#002C49]/50 lg:px-2 lg:py-1.5 xl:px-4 xl:py-3 text-white outline-none' 
                         />
                     </div>
 
                     {/* Create Account */}
-                    <p className='text-center text-lg font-montserrat font-medium text-white'>
+                    <p className='text-center lg:text-sm xl:text-lg font-montserrat font-medium text-white'>
                         Don't have account?{' '}
 
                         <Link
@@ -90,7 +90,7 @@ function Login () {
                     {/* Sign-In Button*/}
                     <Link
                         to='/Dashboard'
-                        className='w-fit rounded-xl bg-[#41B0EC] mx-auto py-2 px-6 text-center text-xl font-montserrat font-semibold text-white hover:bg-white hover:text-[#41B0EC] hover:border hover:border-[#41B0EC]'
+                        className='w-fit lg:rounded-lg xl:rounded-xl bg-[#41B0EC] mx-auto lg:px-4 lg:py-1 xl:px-6 xl:py-2 text-center lg:text-md xl:text-xl font-montserrat font-semibold text-white hover:bg-white hover:text-[#41B0EC] hover:border hover:border-[#41B0EC]'
                     >
                         Sign In
                     </Link>
